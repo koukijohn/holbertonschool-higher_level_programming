b69b99295f20e3c13dce4546ee64851ece2fd139
KoukiJohn <koukijohn@hotmail.com>
2018-04-10 23:04:18 +0000

    added some notes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 for x in range(ord('a'), ord('z') + 1):
     print("{}".format(chr(x)), end="")
-#ord to convert letters to numbers kinda like ascii
-#chr this converts the numb back to ascii
