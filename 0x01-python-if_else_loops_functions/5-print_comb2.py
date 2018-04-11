fda87e1f95fa971109283a3eb673b6b04183b397
KoukiJohn <koukijohn@hotmail.com>
2018-04-11 00:13:53 +0000

    removed comments/notes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x != 99:  # end here so no comma
-        print("{:02d}, ".format(x), end="")  # no new line
-print("{:d}".format(x))  # must be out of for loop because x is 99 after exit
+    if x != 99:
+        print("{:02d}, ".format(x), end="")
+print("{:d}".format(x))
