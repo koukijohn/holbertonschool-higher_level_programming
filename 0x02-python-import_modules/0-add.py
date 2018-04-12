2a991b4fc7c6cf8f17d5fdcd8f8b5831723b164c
KoukiJohn <koukijohn@hotmail.com>
2018-04-12 20:18:01 +0000

    added 0-ad d.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, a + b))
+def main():
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    main()
