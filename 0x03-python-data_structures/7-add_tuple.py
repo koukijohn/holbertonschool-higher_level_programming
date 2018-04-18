7d51f9ff2044b21651afdcdd27d34a72d38e8907
KoukiJohn <koukijohn@hotmail.com>
2018-04-18 03:34:56 +0000

    reworked if statements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,14 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 0:
         firstelem = 0, 0
-    if len(tuple_a) == 1:
+    elif len(tuple_a) == 1:
         firstelem = tuple_a[0], 0
-    if len(tuple_a) == 2:
+    else:
         firstelem = tuple_a[0], tuple_a[1]
     if len(tuple_b) == 0:
         secondelem = 0, 0
-    if len(tuple_b) == 1:
+    elif len(tuple_b) == 1:
         secondelem = tuple_b[0], 0
-    if len(tuple_b) == 2:
+    else:
         secondelem = tuple_b[0], tuple_b[1]
     return firstelem[0] + secondelem[0], firstelem[1] + secondelem[1]
