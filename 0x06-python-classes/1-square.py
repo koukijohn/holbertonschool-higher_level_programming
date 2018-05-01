17725889ee7a0369c2bea451d147e9cfafb57b8b
KoukiJohn <koukijohn@hotmail.com>
2018-05-01 23:58:22 +0000

    added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    """ This is the Square class. """
+    def __init__(self, size):
+        """ This is initializing itself.
+
+        Args:
+            size: This is the size of the square.
+
+        Returns:
+            Nothing.
+
+        """
+
+        self.__size = size
