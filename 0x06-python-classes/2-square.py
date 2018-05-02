2243d310f7d5ab32e1d63d15f4b1b2be1c493c19
KoukiJohn <koukijohn@hotmail.com>
2018-05-02 00:59:40 +0000

    added 2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """ This is the square class. """
+    def __init__(self, size=0):
+        """ init is called to create an object. self refers to itself
+
+        Args:
+            size (int): This is the size of the square.
+
+        Returns:
+            Nothing.
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
