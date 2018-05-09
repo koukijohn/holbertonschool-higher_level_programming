10ade8dc2325d4e39662e83d645c77a9a44349e9
KoukiJohn <koukijohn@hotmail.com>
2018-05-09 21:31:35 +0000

    added 2-rectangle.py:

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -19,6 +19,35 @@ class Rectangle:
         self.height = height
 
     @property
+    def width(self):
+        """ This retrieves the width.
+
+        Args:
+            None.
+
+        Return:
+            None.
+
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ This sets the width equal to the value.
+
+        Args:
+            value: This is the value of the width to set.
+
+        Return:
+            None.
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
     def height(self):
         """ This retrieves the height.
 
@@ -29,7 +58,7 @@ class Rectangle:
             None.
 
         """
-        self.__height = height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -48,52 +77,19 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    @property
-    def width(self):
-        """ This retrieves the width.
-
-        Args:
-            None.
-
-        Return:
-            None.
-
-        """
-        self.__width = width
-
-    @width.setter
-    def width(self, value):
-        """ This sets the width equal to the value.
-
-        Args:
-            value: This is the value of the width to set.
-
-        Return:
-            None.
-        """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     def area(self):
         """ This will find the area of a rectangle.
-
         Args:
             None.
-
         Return:
             None.
         """
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
         """ This will find the perimeter.
-
         Args:
             None.
-
         Return:
             None.
         """
