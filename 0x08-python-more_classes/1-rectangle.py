5a15742009c527203a35c2147a4d3ba31c3bd12e
KoukiJohn <koukijohn@hotmail.com>
2018-05-09 03:03:44 +0000

    changed edge cases for 1-rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,15 +15,7 @@ class Rectangle:
             None.
         """
 
-        if type(width) != int:
-            raise TypeError("width must be an integer")
-        elif width < 0:
-            raise ValueError("width must be >= 0")
         self.__width = width
-        if type(height) is not height:
-            raise TypeError("height must be an integer")
-        elif height < 0:
-            raise ValueError("height must be an integer")
         self.__height = height
 
     @property
