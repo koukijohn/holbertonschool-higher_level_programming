f07fee5d381b1d802ab40f60523bdbee2437a964
KoukiJohn <koukijohn@hotmail.com>
2018-05-16 05:05:30 +0000

    added 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This module ia about inheriting rectangle class into square class. """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ This is a class that inherits from class Rectangle.
+    """
+    def __init__(self, size):
+        """ This instantiaties the size for the square. """
+        self.__size = size
+        super().integer_validator("size", size)
+        super().__init__(size, size)
