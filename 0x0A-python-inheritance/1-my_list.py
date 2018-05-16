7dedcbe6ce3a3f680ebac24242a84a0a830915b0
KoukiJohn <koukijohn@hotmail.com>
2018-05-16 21:24:27 +0000

    fixed comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" This module is about printing the sorted self. """
+
+
 class MyList(list):
-    """ Ths is a class that inherits from list.
+    """ This is a class that inherits from list.
     """
     def print_sorted(self):
         """ This prints the list, but sorted in ascending order.
