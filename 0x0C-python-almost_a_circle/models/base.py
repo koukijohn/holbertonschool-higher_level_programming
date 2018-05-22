29d2d047b653a2cfd1dae8f6f53fb3752c065616
KoukiJohn <koukijohn@hotmail.com>
2018-05-22 22:23:40 +0000

    updated base with all def's

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,48 @@ class Base:
             return ("[]")
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ Writes the JSON string representation of list_objs to a file """
+        with open("{}.json".format(cls.__name__), "w",
+                  encoding="UTF8") as file:
+            list = []
+            if list_objs is None:
+                file.write(Base.to_json_string([]))
+            else:
+                for obj in list_objs:
+                    list.append(obj.to_dictionary())
+                file.write(Base.to_json_string(list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation json_string. """
+        if json_string is None:
+            return ("[]")
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ Returns an instance with all attributes already set. """
+        if cls.__name__ == "Square":
+            x = cls(8, 8, 8, 8)
+        if cls.__name__ == "Rectangle":
+            x = cls(8, 8, 8, 8, 8)
+        x.update(**dictionary)
+        return x
+
+    @classmethod
+    def load_from_file(cls):
+        """ Returns a list of instances. """
+        try:
+            with open("{}.json".format(cls.__name__), "r",
+                      encoding="UTF8") as x:
+                load = json.load(x)
+                instance = []
+                for g in load:
+                    instance.append(cls.create(**g))
+                return instance
+        except FileNotFoundError:
+            return ([])
