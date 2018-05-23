be70dcf46d06d6abcfc1f593867c6fcf5a1d6a9d
KoukiJohn <koukijohn@hotmail.com>
2018-05-23 21:39:23 +0000

    updated base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" This module contains all of the unittest for the Base class. """
 import unittest
 import pep8
 from models.base import Base
@@ -8,56 +9,46 @@ import sys
 from io import StringIO
 import json
 import os
-"""
-This module contains all unittest cases for
-Base class
-"""
 
 
 class TestBase(unittest.TestCase):
-    """
-    Class containing functions to run
-    multiple tests
-    """
+    """ This class contains all of our unittest for Base. """
+
     def setUp(self):
-        """
-        function to redirect stdout to check
-        outpute of functions relying on print
-        """
+        """ This function redirects to stdout and checks
+        the output of functions relying on print. """
+
         sys.stdout = StringIO()
 
     def tearDown(self):
-        """
-        cleans everything up after running
-        setup
-        """
+        """ This tearsDown/cleans everything up after running
+        the setup. """
+
         sys.stdout = sys.__stdout__
 
     def test_pep8_model(self):
-        """
-        Tests for pep8
-        """
+        """ This tests for pep8. """
+
         p8 = pep8.StyleGuide(quiet=True)
         p = p8.check_files(['models/base.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_pep8_test(self):
-        """
-        Tests for pep8
-        """
+        """ This tests for pep8. """
+
         p8 = pep8.StyleGuide(quiet=True)
         p = p8.check_files(['tests/test_models/test_base.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_docstring(self):
-        """test if docstring"""
+        """ This test if d it's a docstring. """
+
         self.assertIsNotNone(Base.__doc__)
 
     def test_00_documentation(self):
-        """
-        Test to see if documentation is
-        created and correct
-        """
+        """ This tests to see if the documentation is
+        created and if it's correct. """
+
         self.assertTrue(hasattr(Base, "__init__"))
         self.assertTrue(Base.__init__.__doc__)
         self.assertTrue(hasattr(Base, "create"))
@@ -72,9 +63,8 @@ class TestBase(unittest.TestCase):
         self.assertTrue(Base.load_from_file.__doc__)
 
     def test_0_id(self):
-        """
-        Test to check for id method
-        """
+        """ This test checks for the id method. """
+
         Base._Base__nb_objects = 0
         b1 = Base()
         b2 = Base()
@@ -88,17 +78,15 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b5.id, 4)
 
     def test_1_id(self):
-        """
-        After run set of ids
-        """
+        """ This test checks after it runs a set of ids. """
+
         Base._Base__nb_objects = 0
         bas = Base()
         self.assertEqual(bas.id, 1)
 
     def test_2_id(self):
-        """
-        Random arguments passed to check
-        """
+        """ This checks to see if the random arguments passed fail or not. """
+
         Base._Base__nb_objects = 0
         t1 = Base(22)
         self.assertEqual(t1.id, 22)
@@ -108,9 +96,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(t3.id, 1)
 
     def test_3_set_nb(self):
-        """
-        setting nb_objects as private
-        """
+        """ This checks to see if nb_objects is set as private. """
+
         b = Base(33)
         with self.assertRaises(AttributeError):
             print(b.nb_objects)
@@ -118,10 +105,9 @@ class TestBase(unittest.TestCase):
             print(b.__nb_objects)
 
     def test_4_dict(self):
-        """
-        Test to check if dictionary
-        is working
-        """
+        """ This test checks to see if the dictionary
+        is working. """
+
         r1 = Rectangle(10, 7, 2, 8, 1)
         d1 = r1.to_dictionary()
         j = {'x': 2, 'id': 1, 'y': 8, 'height': 7, 'width': 10}
@@ -131,10 +117,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(type(jd), str)
 
     def test_6_from_json_string(self):
-        """
-        Test to check from json to string
-        conversion
-        """
+        """ This test checks to see if from_json_to_string works or not. """
+
         s = '[{"id": 9, "width": 10, "height": 11, "x": 12, "y": 13}, \
 {"id": 10, "width": 12, "height": 14, "x": 16, "y": 18}]'
         js = Base.from_json_string(s)
@@ -142,35 +126,31 @@ class TestBase(unittest.TestCase):
         self.assertEqual(len(js), 2)
 
     def test_7_from_json_string_empty(self):
-        """
-        Test to check if it works with
-        empty string or none
-        """
+        """ This test checks to see if it works with
+        an empty string or none. """
+
         self.assertEqual(Base.from_json_string(""), [])
         self.assertEqual(Base.from_json_string(None), [])
 
     def test_10_rect(self):
-        """
-        Test to check for rectangle creation
-        """
+        """ This test checks for rectangle creation. """
+
         R1 = Rectangle(4, 5, 6)
         R1_dict = R1.to_dictionary()
         R2 = Rectangle.create(**R1_dict)
         self.assertNotEqual(R1, R2)
 
     def test_11_sq(self):
-        """
-        Test to check for square creation
-        """
+        """ This test checks for the square creation. """
+
         S1 = Square(44, 55, 66, 77)
         S1_dict = S1.to_dictionary()
         S2 = Rectangle.create(**S1_dict)
         self.assertNotEqual(S1, S2)
 
     def test_12_file_rect(self):
-        """
-        Test to check if file loads from rect
-        """
+        """ This test checks if the file loads from rect. """
+
         R1 = Rectangle(33, 34, 35, 26)
         R2 = Rectangle(202, 2)
         lR = [R1, R2]
@@ -179,9 +159,8 @@ class TestBase(unittest.TestCase):
         self.assertNotEqual(lR, lR2)
 
     def test_13_file_square(self):
-        """
-        Test to check if file loads from square
-        """
+        """ This test checks to see if the file loads from square. """
+
         S1 = Square(22)
         S2 = Square(44, 44, 55, 66)
         lS = [S1, S2]
