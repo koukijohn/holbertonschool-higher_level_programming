6c22c675b9b7e208da656a7351c4dd5c6b688b6b
KoukiJohn <koukijohn@hotmail.com>
2018-06-26 01:07:14 +0000

    updated version

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,3 @@
 -- This is a script that lists the number of records with the same score in the
 -- table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT SCORE, count(*) AS number FROM second_table GROUP BY score ORDER BY
-score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score DESC;
