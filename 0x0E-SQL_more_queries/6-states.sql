de01841537d5660887b5ae9433578a42c265e2f4
koukijohn <koukijohn@github.com>
2018-06-27 04:37:09 +0000

    added 6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- This is a script that creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa) on our MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS states (id INT AUTO_INCREMENT UNIQUE NOT
+NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
