a4bf98e482f61564b86e48909461ae75fd2f1dd1
koukijohn <koukijohn@github.com>
2018-06-27 05:02:42 +0000

    fixed comment

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,4 @@
--- This is a script that lists all cities contained in database hbtn_0d_usa.
+-- This is a script that lists all cities contained in database
+-- hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name FROM cities, states
 WHERE cities.state_id = state.id;
