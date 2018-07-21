b598cada9c4c4ab7e7f1654658de7de578fa2be4
koukijohn <koukijohn@github.com>
2018-07-21 03:25:52 +0000

    fixed pep8

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    states = session.query(State).filter(State.name.contains("a")).\
+        order_by(State.id).all()
+
+    for letter in states:
+        print("{}: {}".format(letter.id, letter.name))
+
+    session.close()
