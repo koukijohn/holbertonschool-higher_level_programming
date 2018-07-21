5aab7137cdf88ee7c731e4a884f15bd5af44895c
koukijohn <koukijohn@github.com>
2018-07-21 04:44:19 +0000

    added 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This script lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base.metadata.create_all(engine)
+
+    state_change = session.query(State).filter(State.id == 2).first()
+    state_change.name = "New Mexico"
+
+    session.commit()
+
+    session.close()
