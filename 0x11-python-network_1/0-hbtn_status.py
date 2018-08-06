0fbea291db679841bcc0d8e1f447829e2e1a29f0
koukijohn <koukijohn@github.com>
2018-08-06 17:52:58 +0000

    added 0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    This is a python script that fetches https://intranet.hbtn.io/status w/
+        - urllib
+"""
+
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    url = 'https://intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    utf8 = html.decode('utf-8')
+print("Body response:")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(utf8))
