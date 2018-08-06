dd908ee6534ae987929c0938dc2816df11a72efd
koukijohn <koukijohn@github.com>
2018-08-06 17:55:19 +0000

    testing reformat

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,12 +9,12 @@ import urllib.request
 
 if __name__ == "__main__":
 
-    url = 'https://intranet.hbtn.io/status'
+    # url = 'https://intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    utf8 = html.decode('utf-8')
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(utf8))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        utf8 = html.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(utf8))
