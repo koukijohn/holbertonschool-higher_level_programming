62941c41604fa421dda5fb621b57ec6a0b6cdca7
koukijohn <koukijohn@github.com>
2018-08-06 17:57:07 +0000

    stored url in variable url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,9 +9,9 @@ import urllib.request
 
 if __name__ == "__main__":
 
-    # url = 'https://intranet.hbtn.io/status'
+    url = "https://intranet.hbtn.io/status"
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen(url) as response:
         html = response.read()
         utf8 = html.decode('utf-8')
     print("Body response:")
