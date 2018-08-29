0335e34c7293d8a21597bcef6bea89534c778c90
koukijohn <koukijohn@github.com>
2018-08-29 01:14:59 +0000

    made script more readable

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const iterations = process.argv[2]
+const iterations = process.argv[2];
 if (parseInt(iterations) > 0) {
   for (let x = 0; x < iterations; x++) {
     console.log('X'.repeat(iterations));
