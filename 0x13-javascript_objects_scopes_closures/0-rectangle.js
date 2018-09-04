3ecaf624111541c92939f231513d78d2bf288cf8
koukijohn <koukijohn@github.com>
2018-09-04 18:05:54 +0000

    added 0-rectangle.js

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // This module contains an empty class Rectangle that defines a rectangle.
 // module.exports =
-class Rectangle {};
+class Rectangle {}
 module.exports = Rectangle;
