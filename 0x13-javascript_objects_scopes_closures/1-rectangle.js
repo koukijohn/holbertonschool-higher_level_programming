53781632c294b144998d998885b800629ab15e67
koukijohn <koukijohn@github.com>
2018-09-04 18:18:46 +0000

    changed width and height to w and h

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // This is a module that contains a class Rectangle that defines a rectangle
 class Rectangle {
-  constructor (width, height) {
-    this.width = width;
-    this.height = height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
