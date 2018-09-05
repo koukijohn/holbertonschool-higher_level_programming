44816a9bd596fac6849027808d1e4224e3090a29
koukijohn <koukijohn@github.com>
2018-09-05 21:18:50 +0000

    changed method of checking for id of character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,20 @@
 // This is a script that prints the number of movies where the character
 // "Wedge Antilles" is present.
 
-let URL = 'https://swapi.co/api/people/18/';
 let request = require('request');
 request(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
-  } else if (response.statusCode === 200){
+  } else {
     let results = JSON.parse(body).results;
-    let movies = 0;
-    for (let x = 0; x < results.length; x++) {
-      let characters = results[x].characters;
-      for (let y = 0; y < characters.length; y++) {
-        if (characters[y] === URL) {
-          movies += 1;
+    let appearances = 0;
+    for (let movieTitle of results) {
+      for (let characterID of movieTitle.characters) {
+        if (characterID.search('/18/') > 0) {
+          appearances += 1;
         }
       }
     }
-    console.log(movies);
+    console.log(appearances);
   }
 });
