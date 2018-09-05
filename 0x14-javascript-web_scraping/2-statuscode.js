91daeffab1e00614994e24fbbdf658559352b720
koukijohn <koukijohn@github.com>
2018-09-05 19:46:04 +0000

    added 2-statuscode.js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This is a script that will display the status code of a GET request.
+let request = require('request');
+
+request(process.argv[2], (error, response) => {
+  if (error) {
+    console.log('code:', error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
+});
