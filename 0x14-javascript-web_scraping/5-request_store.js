e2fba540241783e1f62418333e63ea8a1d051bdf
koukijohn <koukijohn@github.com>
2018-09-05 22:43:36 +0000

    added 5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// This script will get the contents of a webpage and store it in a file.
+
+let request = require('request');
+
+request(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else { // response == 200
+    let written = body;
+    let fs = require('fs');
+    fs.writeFile(process.argv[3], written, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
