b366f60e602a23e09b3da8e3cfb0dd124f16a5d5
koukijohn <koukijohn@github.com>
2018-09-07 23:34:27 +0000

    added 2-script.js

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 // This script will turn red when clicked.
 // This is a javascript script that will update the text color of the HEADER
 // HTML to red (#FF0000) when a user clicks on the tag 'DIV#red_header'
