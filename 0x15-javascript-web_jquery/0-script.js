b424453ce273176dbcb0abb69b17f4b71e6a7253
koukijohn <koukijohn@github.com>
2018-09-07 21:21:04 +0000

    added 0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// This javascript script will update the text color of the HEADER HTML tag.
+// to the color red. #FF0000
+document.querySelector('header').style.color = '#FF0000';
