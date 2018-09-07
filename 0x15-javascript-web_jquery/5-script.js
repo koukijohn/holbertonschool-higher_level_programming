e25e01375d1bf0c2b345d050bf2946583409e71a
koukijohn <koukijohn@github.com>
2018-09-07 23:19:59 +0000

    added 5-script.js

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// This script adds a LI element to a list when the user clicks on the tag
+// DIV#add_item
+$('DIV#add_item').click(function () {
+  $('<li>Item</li>').appendTo('UL.my_list');
+});
